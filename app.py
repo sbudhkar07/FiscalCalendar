87931ee50745f83e12afbbb2aeaa1c7c6eafcc40
Seyaan Budhkar <seyaanbudhkar@gmail.com>
2024-06-05 09:47:00 +0100

    Flask App

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
+from flask import Flask, render_template, request
 import sqlite3
+from datetime import date, datetime
+import calendar
 
-conn = sqlite3.connect("database.db")
-cursor = conn.cursor()
-cursor.execute("select * from events")
-rows = cursor.fetchall()
-conn.close()
-for row in rows:
-    print (row)
+app = Flask(__name__)
+
+
+def current_month_tasks():
+    month = datetime.now().month
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    cursor.execute(f'''
+    select TaskName, Day 
+    from events, tasks 
+    where events.TaskID == tasks.TaskID 
+    and Month == {month}''')
+    tasks = cursor.fetchall()
+    conn.close()
+    return tasks
+
+@app.route('/')
+def calendar_view():
+    tasks = current_month_tasks()
+    today = date.today()
+    calendar = calendar.Calendar()
+    days = calendar.monthdatescalendar(today.year, today.month)
+
+    return render_template('index.html', tasks=tasks, month=days)
+
+if __name__ == '__main__':
+    app.run(debug=True)
